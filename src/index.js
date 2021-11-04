5b6a8d61ed7ed477f022db9f8e3eb1dac66e3c3b
Aastha-Varma <aastha.code@gmail.com>
2021-11-04 19:58:12 +0530

    Add Winner logic function

--- src/index.js
+++ src/index.js
@@ -38,22 +38,29 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext? 'X': 'O');
-  
-      return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = 'Next player: ' + (this.state.xIsNext? 'X': 'O');
+        }
+         
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-                {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
-            </div>
-        </div>
-      );
+        );
     }
   }
   
@@ -71,6 +78,27 @@ function Square(props) {
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+      const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7], 
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+      ];
+
+      for (let line of lines){
+          const [a, b, c] = line;
+          if(squares[a] && squares[a]==squares[b] && squares[a]==squares[c]) {
+              return squares[a];
+          }
+      }
+      return null;
   }
   
   // ========================================
